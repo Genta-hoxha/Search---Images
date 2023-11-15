ab449a6e99ebace39960ec5e432b13cd965c362d
Genta-hoxha <genta.hoxha10@gmail.com>
2023-11-15 12:38:35 +0100

    modal image

--- script.js
+++ script.js
@@ -65,31 +65,73 @@ function checkData() {
 
 const displayCard = document.getElementById("container");
 displayCard.innerHTML = "";
+
+const modalHtml = `
+  <div class="modal">
+    <span class="close">&times;</span>
+    <img class="modal-content" id="img01" style="background-color: transparent; border-color: transparent">
+    <div id="caption"></div>
+  </div>
+`;
+displayCard.insertAdjacentHTML("beforeend", modalHtml);
 // displayCard.insertAdjacentHTML("beforeend", html);
 if (Array.isArray(dataJson.photos)) {
   dataJson.photos.forEach((photo) => {
     const likeImage = photo.liked ? "/img/Love.png" : "/img/Love_Heart.png";
     const html = `
     <div class="card" style = "height:550px">
-      <img class="card-img-top" src="${photo.src.original}" alt="${photo.alt}" />
-      <button class="heart"    style="padding-right: 260px">
+      <img class="card-img-top" id="image" src="${photo.src.original}" alt="${photo.alt}" />
+   
+          <button class="heart"    style="padding-right: 260px">
     
       <img src="${likeImage}" style="left: -170px; width: 40px; height: 40px;" />
       </button>
       <div class="card-body" style=" width: 65%;
       text-align: left;
-      font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif">
-        <h4 class="card-title" style = "margin-bottom:-290px">${photo.photographer}</h4>
-        <p class="card-text" style = "margin-bottom:-290px">
+      font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif">   </div>
+      <div>
+        <h4 class="card-title" >${photo.photographer}</h4></div>
+       <div> <p class="card-text" >
           <span>Description:</span> ${photo.alt}
-        </p>
-      </div>
+        </p></div>
+   
     </div>
   `;
 
     // Append the HTML to the container
     displayCard.insertAdjacentHTML("beforeend", html);
   });
+  const images = document.querySelectorAll("#image");
+  const modal = document.querySelector(".modal");
+  const modalImg = document.getElementById("img01");
+  const captionText = document.getElementById("caption");
+  const closeModalBtn = document.querySelector(".close");
+
+  images.forEach((image) => {
+    image.addEventListener("click", function () {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    });
+  });
+
+  closeModalBtn.onclick = function () {
+    modal.style.display = "none";
+  };
 } else {
   console.error("dataJson.photos is not an array.");
 }
+
+// let modal = document.getElementById("myModal");
+// let img = document.getElementById("image");
+// let modalImg = document.getElementById("img01");
+// let captionText = document.getElementById("caption");
+// img.onclick = function () {
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// };
+// let span = document.getElementsByClassName("close")[0];
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
